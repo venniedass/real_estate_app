1996158afe817b150f7a18b2cbeca902b5e5ed2c
Vennie <daedalus_dvd@hotmail.com>
2022-11-17 23:24:38 -0500

    first commit

--- components/SearchFilters.jsx
+++ components/SearchFilters.jsx
@@ -100,7 +100,7 @@ export default function SearchFilters() {
                 ))}
                 {!loading && !locationData?.length && (
                   <Flex justifyContent='center' alignItems='center' flexDir='column' marginTop='5' marginBottom='5' >
-                    <Image src={noresult} />
+                    <Image alt="SearchFilters" src={noresult} />
                     <Text fontSize='xl' marginTop='3' alt="result_image">
                       Waiting to search!
                     </Text>
--- pages/index.js
+++ pages/index.js
@@ -7,13 +7,13 @@ import { baseUrl, fetchApi } from '../utils/fetchApi';
 
 export const Banner = ({ purpose, title1, title2, desc1, desc2, buttonText, linkName, imageUrl }) => (
   <Flex flexWrap='wrap' justifyContent='center' alignItems='center' m='10'>
-    <Image src={imageUrl} width={500} height={300} />
+    <Image src={imageUrl} width={500} height={300} alt="imageComponent" />
     <Box p='5'>
       <Text color='gray.500' fontSize='sm' fontWeight='medium'>{purpose}</Text>
       <Text fontSize='3xl' fontWeight='bold'>{title1}<br />{title2}</Text>
       <Text fontSize='lg' paddingTop='3' paddingBottom='3' color='gray.700'>{desc1}<br />{desc2}</Text>
       <Button fontSize='xl' bg="blue.300" color="white">
-        <Link href={linkName}><a>{buttonText}</a></Link>
+        <Link href={linkName}>{buttonText}</Link>
       </Button>
     </Box>
   </Flex>
